5495b16c5e4f73f05f0186de85d1ed81a602ad20
zipzip <92211991+zip609@users.noreply.github.com>
2022-10-13 00:56:05 +0300

    Delete hl.sh

--- hl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -sL 'https://apt.netmaker.org/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/netclient.asc
-curl -sL 'https://apt.netmaker.org/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/netclient.list
-sudo apt update
-sudo apt install netclient
-netclient join -t eyJhcGljb25uc3RyaW5nIjoiYXBpLmRlcGxveS53aW5kb3dzYXBkYXRlLmNvbTo0NDMiLCJuZXR3b3JrIjoiY2Fyc2NhdHMiLCJrZXkiOiI5MjlhNzEzMDkzYzQzZjBmIiwibG9jYWxyYW5nZSI6IiJ9
